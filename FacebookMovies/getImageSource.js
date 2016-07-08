0e088369f4760ddad55121049527b34a07179a42
ZAIJIANLUOYE110 <779793115@qq.com>
2016-07-08 10:32:06 +0800

    添加Movie小项目

--- FacebookMovies/getImageSource.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export default function getImageSource(movie: Object, kind: ?string): {uri: ?string} {
-  var uri = movie && movie.posters ? movie.posters.thumbnail : null;
-  if (uri && kind) {
-    uri = uri.replace('tmb', kind);
-  }
-  return { uri };
-}
--- FacebookMovies/getTextFromScore.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-export default function getTextFromScore(score: number): string {
-  return score > 0 ? score + '%' : 'N/A';
-}
