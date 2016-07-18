273bd9ac7458dc1165493d758a22a7281da02738
ZAIJIANLUOYE110 <779793115@qq.com>
2016-07-18 20:41:03 +0800

    添加‘微博’cell，不过还没写代码

---
+++ SinaWebBo/Home/StatusCell.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  Image,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
+
+export default class StatusCell extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
