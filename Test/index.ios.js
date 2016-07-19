a0a892e2dd67523c1cb9ccb56b2c5d2f8031dd7d
ZAIJIANLUOYE110 <779793115@qq.com>
2016-07-19 19:08:34 +0800

    nothing

--- Test/index.ios.js
+++ Test/index.ios.js
@@ -17,47 +17,12 @@ import Person from './Person';
 class Test extends Component {
   constructor(props) {
     super(props);
-    // let p1 = new Person();
-    // console.log(Person.instCount);
-    // let p2 = new Person();
-    // console.log(Person.instCount);
-    //
-    // // p1.name = 'zwr';
-    // // p2.name = 'zwr2';
-    // // p1.sayHello();
-    // // p2.sayHello();
-    // console.log(Person.name1);
-    // // console.log(Object.getOwnPropertyDescriptor(p1, 'name1'));
-
-    var sleep = (time) => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve('done');
-        }, time);
-      });
-    };
-
-    var start = async () => {
-      console.log(await sleep(3000));
-      console.log('start end');
-    };
-
-    start();
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        
       </View>
     );
   }
