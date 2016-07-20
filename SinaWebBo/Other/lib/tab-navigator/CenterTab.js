46d308c83a1c8977fb920c0ab06246d8292665fb
ZAIJIANLUOYE110 <779793115@qq.com>
2016-07-20 13:25:04 +0800

    完成tabBar中间按钮的

---
+++ SinaWebBo/Other/lib/tab-navigator/CenterTab.js
@@ -0,0 +1,109 @@
+// edit by glm
+// 中间按钮
+'use strict';
+import React, {
+  PropTypes,
+} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import Layout from './Layout';
+
+export default class Tab extends React.Component {
+  static propTypes = {
+    testID : PropTypes.string,
+    title: PropTypes.string,
+    titleStyle: Text.propTypes.style,
+    badge: PropTypes.element,
+    onPress: PropTypes.func,
+    hidesTabTouch: PropTypes.bool,
+    allowFontScaling: PropTypes.bool,
+    style: View.propTypes.style,
+  };
+
+  constructor(props, context) {
+    super(props, context);
+
+    this._handlePress = this._handlePress.bind(this);
+  }
+
+  render() {
+    let { title, badge } = this.props;
+    let icon = null;
+    if (React.Children.count(this.props.children) > 0) {
+      icon = React.Children.only(this.props.children);
+    }
+
+    if (title) {
+      title =
+        <Text
+          numberOfLines={1}
+          allowFontScaling={!!this.props.allowFontScaling}
+          style={[styles.title, this.props.titleStyle]}>
+          {title}
+        </Text>;
+    }
+
+    if (badge) {
+      badge = React.cloneElement(badge, {
+        style: [styles.badge, badge.props.style],
+      });
+    }
+
+    {/* 这里是与Tab.js不同的地方
+        tabStyle去掉了：title ? null : styles.untitledContainer，
+        只是为了让中间的十字按钮居中 */}
+    let tabStyle = [
+      styles.container,
+      this.props.style,
+    ];
+    return (
+      <TouchableOpacity
+        testID={this.props.testID}
+        activeOpacity={this.props.hidesTabTouch ? 1.0 : 0.8}
+        onPress={this._handlePress}
+        style={tabStyle}>
+        <View>
+          {icon}
+          {badge}
+        </View>
+        {title}
+      </TouchableOpacity>
+    );
+  }
+
+  _handlePress(event) {
+    if (this.props.onPress) {
+      this.props.onPress(event);
+    }
+  }
+}
+
+let styles = StyleSheet.create({
+  badge: {
+    position: 'absolute',
+    top: -6,
+    right: -10,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  untitledContainer: {
+    paddingBottom: 13,
+  },
+  title: {
+    color: '#929292',
+    fontSize: 10,
+    textAlign: 'center',
+    alignSelf: 'stretch',
+    marginTop: 4,
+    marginBottom: 1 + Layout.pixel,
+  },
+});
