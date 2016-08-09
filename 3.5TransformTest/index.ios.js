4468380bc41ca722bec6741bb021f2a45b878330
coderGLM <779793115@qq.com>
2016-08-09 22:19:07 +0800

    transfrom scale test

---
+++ 3.5TransformTest/index.ios.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+class TransformTest extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={{
+            width: 200,
+            height: 200,
+          }}
+          resizeMode='contain'
+          source={{uri: 'http://d.hiphotos.baidu.com/zhidao/pic/item/0df3d7ca7bcb0a4609b3f9c16a63f6246a60afb1.jpg'}}
+          >
+        </Image>
+        <Image
+          style={{
+            width: 200,
+            height: 200,
+            transform: [
+              {scale: 2}
+            ],
+          }}
+          resizeMode='contain'
+          source={{uri: 'http://d.hiphotos.baidu.com/zhidao/pic/item/0df3d7ca7bcb0a4609b3f9c16a63f6246a60afb1.jpg'}}
+          >
+        </Image>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+AppRegistry.registerComponent('TransformTest', () => TransformTest);
